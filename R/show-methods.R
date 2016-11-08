e528e5f69a8b76051b60d23134b2894523ae26ae
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-11-08 16:14:56 +0100

    show methods in separate file

---
+++ R/show-methods.R
@@ -0,0 +1,53 @@
+################################################################################
+#' @rdname show-methods
+setMethod("show", "PairedReadFileSet", function(object) {
+    cat("  A ", class(object),
+        " instance of length ", length(object@readsF), 
+        "\n", sep = "")
+    if (length(object@readsF) != 0) {
+        cat("\n", length(object@readsF), " Forward read files:",
+            substr(paste(object@readsF, collapse=" "), 1, 180),
+            "... \n")
+        cat(length(object@readsR), " Reverse read files:",
+            substr(paste(object@readsR, collapse=" "), 1, 180),
+            "... \n")
+    }
+})
+
+################################################################################
+#' @rdname show-methods
+setMethod("show", "PrimerPairsSet", function(object){
+    cat("  A ", class(object),
+        " instance of length ", length(object@primerF), 
+        "\n", sep = "")
+    if (length(object@primerF) != 0) {
+        cat("Forward:\n")
+        cat(Biostrings:::.XStringSet.show_frame(object@primerF))
+        cat("Reverse:\n")
+        cat(Biostrings:::.XStringSet.show_frame(object@primerR))
+    }
+})
+
+
+################################################################################
+#' @rdname show-methods
+setMethod("show", "MultiAmplicon", function(object){
+    cat("  A ", class(object),
+        " instance of dimensions ", length(object@PrimerPairsSet), " x ",
+        length(object@PairedReadFileSet),
+        "\n", sep = "")
+    if (length(object@PairedReadFileSet) != 0 &&
+        length(object@PrimerPairsSet) !=0) {
+        cat("Forward Primers:\n")
+        cat(Biostrings:::.XStringSet.show_frame(object@PrimerPairsSet@primerF))
+        cat("Reverse Primers:\n")
+        cat(Biostrings:::.XStringSet.show_frame(object@PrimerPairsSet@primerR))
+        cat("\n", length(object@PairedReadFileSet), " Forward read files:",
+            substr(paste(object@PairedReadFileSet@readsF, collapse=" "), 1, 180),
+            "... \n")
+        cat(length(object@PairedReadFileSet), " Reverse read files:",
+            substr(paste(object@PairedReadFileSet@readsR, collapse=" "), 1, 180),
+            "... \n")
+    }
+})
+
