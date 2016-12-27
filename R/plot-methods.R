90885743fbdb98900a4d6de4654cb3836a67b422
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-12-27 10:21:44 +0100

    first plot method for raw counts per amplicon

---
+++ R/plot-methods.R
@@ -0,0 +1,31 @@
+##' Plot the raw counts for primer pairs matched in different sample
+##' files as a heatmap
+##'
+##' The function uses pheatmap to plot the primer matching statistics
+##' stored in the rawCounts slot of a MultiAmplicons-class object
+##' 
+##' @title plot_Amplicon_numbers
+##' 
+##' @param MAmatrix The primer matching (amplicon) matrix of a
+##'     MultiAmplicon object.
+##' 
+##' @param transf transformation to be applied, recommended default is
+##'     log10
+##' 
+##' @param add offset added to allow e.g. log10 transformation of zero
+##'     values
+##'
+##' @importFrom pheatmap pheatmap
+##' @param ... addtional parameter to be passed to pheatmap funciton
+##' @return
+##' @author Emanuel Heitlinger
+
+plot_Amplicon_numbers <- function (MAmatrix, transf=log10, add=0.1, ...){
+    if (nrow(MA@rawCounts) < 2 || ncol(MA@rawCounts) < 2) {
+        stop(cat("No rawCounts found in MultiAmplicon object:
+                  Run sortAmplicons to produce a MultiAmplicon with at least
+                  two files and two samples"))
+    } else {
+        pheatmap(transf(MAmatrix + add), ...)
+    }
+}
