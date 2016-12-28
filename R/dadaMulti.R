0db0542b94ee20dcd99d6d8c76c77b45194ea553
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-12-28 11:46:53 +0100

    import/export for MultiDada

--- R/dadaMulti.R
+++ R/dadaMulti.R
@@ -1,21 +1,27 @@
-##
-##' .. content for \description{} (no empty lines) ..
+##' A wrapper around \link{\code{dada}} from \link{\code{dada2}}
 ##'
-##' .. content for \details{} ..
+##' The function runs \link{\code{dada}} from \link{\code{dada2}} to
+##' performe 'High resolution sample inference from amplicon data' on
+##' multiple amplicons stored as dereplicated sequences in a
+##' MultiAmplicon-class object
+##' 
 ##' @title dadaMulti
-##' @param MA MultiAmplicon object
-##' @param ... Parameters to be passed to dada2::dada 
+##' @param MA MultiAmplicon-class object
+##' @param ... additional parameters to be passed to
+##'     \code{\link{dada}} from \code{\link{dada}}
 ##' @return MultiAmplicon object with dadaF and dadaR slots filled
+##' @importFrom dada2 dada
+##' @export
 ##' @author Emanuel Heitlinger
 
 
 dadaMulti <- function(MA, ...){
     dada <- lapply(seq_along(MA@PrimerPairsSet), function (i){
         cat("amplicon", rownames(MA)[i], "dada estimateion of sequence variants from ",
-            length(MA@dada[[i]]@derepF), " of ",
+            length(MA@derep@derepF[i]), " of ",
             ncol(MA), "possible sample files\n")
-        dadaF <- dada(MA@dada[i]@derepF, ...)
-        dadaR <- dada(MA@dada[i]@derepR, ...)
+        dadaF <- dada(MA@derep@derepF[i], ...)
+        dadaR <- dada(MA@derep@derepR[i], ...)
         new("PairedDada",
             dadaF = dadaF,
             dadaR = dadaR,
