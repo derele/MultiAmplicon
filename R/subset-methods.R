a15e11a8130d559f271c03cb101c0092d2939c00
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-12-28 11:25:57 +0100

    Further steps towards efficient subsetting up to stratifiedFiles

--- R/subset-methods.R
+++ R/subset-methods.R
@@ -1,10 +1,10 @@
-setMethod("[", "PrimerPairsSet", function(x, i){
+setMethod("[", c("PrimerPairsSet", "ANY"),  function(x, i){
     newF <- x@primerF[i]
     newR <- x@primerR[i]
     PrimerPairsSet(as.character(newF), as.character(newR))
 })
 
-setMethod("[", "PairedReadFileSet", function(x, i){
+setMethod("[", c("PairedReadFileSet", "ANY"), function(x, i){
     newF <- x@readsF[i]
     newR <- x@readsR[i]
     PairedReadFileSet(newF, newR)
@@ -12,25 +12,35 @@ setMethod("[", "PairedReadFileSet", function(x, i){
 
 ##' Convenient subsetting for MultiAmplicon objects
 ##'
+##' Subset a MultiAmplicon object including all potentially filled
+##' slots
 ##' 
 ##' @title subset MultiAmplicon
 ##' @param MultiAmplicon-class object
 ##' @param i numeric, logical or names vector for subsetting rows (==
 ##'     amplicons)
 ##' @param j numeric, logical or names vector for subsetting columns
-##'     (== read files =~ samples)
-##' @return
+##'     (== read files, corresponding usually to samples)
+##' @param drop should 
+##' @return a subset of the original MultiAmplicon object
+##' @export
 ##' @author Emanuel Heitlinger
-setMethod("[", "MultiAmplicon", function(x, i=TRUE, j=TRUE){
-    newPrimer <- x@PrimerPairsSet[i]
-    newFiles <- x@PairedReadFileSet[j]
-    if(any(dim(x@rawCounts)>0)){
-        newRC <- rawCounts(x)[i,j]
-    } else{newRC <- matrix()}
-    new("MultiAmplicon",
-        PrimerPairsSet = newPrimer,
-        PairedReadFileSet = newFiles,
-        rawCounts = newRC
-        )
-})
+setMethod("[", c("MultiAmplicon", "ANY", "ANY", "logical"),
+          function(x, i=TRUE, j=TRUE, drop=FALSE){
+              newPrimer <- x@PrimerPairsSet[i, drop=drop]
+              newFiles <- x@PairedReadFileSet[j, drop=drop]
+              if(any(dim(x@rawCounts)>0)){
+                  newRC <- as.matrix(rawCounts(x)[i, j, drop=drop])
+##                   newSF <- x@stratifiedFiles[i] ## FIXME wrong
+                  ## because j are
+                  ## dropped without
+                  ## placeholder!!
+              } else{newRC <- matrix()}
+              new("MultiAmplicon",
+                  PrimerPairsSet = newPrimer,
+                  PairedReadFileSet = newFiles,
+                  rawCounts = newRC #,
+##                  stratifiedFiles = newSF
+                  )
+          })
 
