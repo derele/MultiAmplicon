b4e43a0a9d9edb1a43a30259eed999017e29e26d
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2020-11-30 11:54:39 +0100

    No idea what goes wrong in calcPropMerged (fails only on TravisCI?)

--- R/pipeline.R
+++ R/pipeline.R
@@ -342,8 +342,9 @@ setMethod("calcPropMerged", "MultiAmplicon",
                       sum(unlist(sapply(x, sgt, simplify=FALSE)))
                   } else {0}
               }
-              nMerged <- sapply(getMergers(MA), getN)
-              nBefore <- sapply(getDadaF(MA), getN)
+              nMerged <- sapply(getMergers(MA, simplify=FALSE), getN)
+              nBefore <- sapply(getDadaF(MA, simplify=FALSE), getN)
+              cat("\n\nNmerged: ", nMerged, "nBefore:", nBefore, "\n\n")
               prop <- nMerged/nBefore
               prop[is.nan(prop)] <- 0
               prop
